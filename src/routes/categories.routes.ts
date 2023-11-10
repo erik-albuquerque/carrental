2135c3f82fb8539bc6bcdbf48c0c1d0e46d02ec3
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-11-10 15:26:33 -0300

    refactor: add list categories use case

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,19 +1,16 @@
 import { Router } from 'express'
 
-import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository'
 import { createCategoryController } from '../modules/cars/useCases/createCategory'
+import { listCategoriesController } from '../modules/cars/useCases/listCategories'
 
 const categoriesRoutes = Router()
-const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', async (request, response) => {
   return createCategoryController.handle(request, response)
 })
 
-categoriesRoutes.get('/', async (req, res) => {
-  const categories = categoriesRepository.list()
-
-  return res.status(200).json({ categories })
+categoriesRoutes.get('/', async (request, response) => {
+  return listCategoriesController.handle(request, response)
 })
 
 export { categoriesRoutes }
