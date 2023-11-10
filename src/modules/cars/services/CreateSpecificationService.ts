40f6d8b27d966af1ee667c009b5b95b6d9fb43a5
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-11-10 14:16:00 -0300

    feat: add create specification route

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,25 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository'
+
+interface IRequestPayload {
+  name: string
+  description: string
+}
+
+class CreateSpecificationService {
+  constructor(private specificationRepository: ISpecificationsRepository) {}
+
+  execute({ name, description }: IRequestPayload): void {
+    const retrievedSpecification = this.specificationRepository.findByName(name)
+
+    if (retrievedSpecification) {
+      throw new Error('Specification already exists!')
+    }
+
+    this.specificationRepository.create({
+      name,
+      description,
+    })
+  }
+}
+
+export { CreateSpecificationService }
