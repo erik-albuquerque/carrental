65535083a5f36a4bd782f2ab54bdb546d3731bc4
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-11-09 21:53:43 -0300

    chore: setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    camelcase: 'off',
+    'import/no-unresolved': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true,
+        },
+      },
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', '/^@shared/', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+    'no-useless-constructor': 'off',
+    'class-methods-use-this': 'off',
+    'lines-between-class-members': 'off',
+    'import/prefer-default-export': 'off',
+    'prettier/prettier': 'error',
+  },
+  settings: {
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.d.ts'],
+    },
+  },
+}
