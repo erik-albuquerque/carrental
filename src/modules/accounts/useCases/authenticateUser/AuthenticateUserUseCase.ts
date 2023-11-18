7d0c77206c3d5f441a3ac90c634fd31cbcff1f60
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-11-17 23:31:10 -0300

    feat: add user authenticate

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,60 @@
+import { compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+import { inject, injectable } from 'tsyringe'
+
+import { IUsersRepository } from '../../repositories/IUsersRepository'
+
+interface IRequestPayload {
+  email: string
+  password: string
+}
+
+interface IResponsePayload {
+  user: {
+    name: string
+    email: string
+  }
+  token: string
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({
+    email,
+    password,
+  }: IRequestPayload): Promise<IResponsePayload> {
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new Error('Email or password incorrect!')
+    }
+
+    const isPasswordMatched = await compare(password, user.password)
+
+    if (!isPasswordMatched) {
+      throw new Error('Email or password incorrect!')
+    }
+
+    const token = sign({}, 'privatekey', {
+      subject: user.id,
+      expiresIn: '1d',
+    })
+
+    const tokenResponse: IResponsePayload = {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    }
+
+    return tokenResponse
+  }
+}
+
+export { AuthenticateUserUseCase }
